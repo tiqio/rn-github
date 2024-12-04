83f31f6a17fe55ddd35cc542a83de1353737058f
dd <dd@mixwan.net>
2024-12-04 22:06:02 +0800

    v1.5 仿照v1.4在HomePage的TrendingPage中加入TabNavigator

--- GitHub_RN_JS/js/page/TrendingPage.js
+++ GitHub_RN_JS/js/page/TrendingPage.js
@@ -1,17 +1,30 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { tabNav } from "../navigator/NavigationDelegate";
+import keys from "../res/data/langs.json";
 export default class Index extends Component {
     render() {
+        const TabNavigator = keys.length ?
+            tabNav({ Component: TrendingTab, theme: { themeColor: '#2196f3' }, keys }) : null;
         return (
             <View style={styles.container}>
-                <Text>TrendingPage</Text>
+                {TabNavigator}
             </View>
         );
+    }
+}
+
+class TrendingTab extends Component {
+    render() {
+        const { tabLabel } = this.props;
+        return (<Text>{tabLabel}</Text>)
     }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        paddingTop: 40,
+        backgroundColor: '#2196f3',
     }
 })
