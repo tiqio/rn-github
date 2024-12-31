1ea5cabe5b0df97a62f9d8b15430f1b0e1c05291
dd <dd@mixwan.net>
2024-12-31 23:28:16 +0800

    v1.7: 通过三方库react-native-safe-area-context成功控制顶部状态栏

--- GitHub_RN_JS/index.js
+++ GitHub_RN_JS/index.js
@@ -7,5 +7,5 @@ import App from "./js/App";
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
-// AppRegistry.registerComponent('GitHub_RN', () => App);
+// registerRootComponent(App);
+AppRegistry.registerComponent('GitHub_RN', () => App);
--- GitHub_RN_JS/js/page/HomePage.js
+++ GitHub_RN_JS/js/page/HomePage.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
 import NavigationUtil from "../navigator/NavigationUtil";
 import DynamicTabNavigator from "../navigator/DynamicTabNavigator";
+import { StatusBar } from "react-native";
+import { SafeAreaProvider, initialWindowMetrics } from 'react-native-safe-area-context';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { connect } from "react-redux";
 
-export default class Index extends Component {
+class HomePage extends Component {
     render() {
         const {theme} = this.props;
         NavigationUtil.navigation = this.props.navigation;
-        return <DynamicTabNavigator/>
+        return (
+            <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+                <SafeAreaView style={{ flex: 1 }} edges={["top"]}>
+                    <StatusBar translucent={true} backgroundColor={theme} />
+                    <DynamicTabNavigator/>
+                </SafeAreaView>
+            </SafeAreaProvider>
+        )
     }
 }
+
+const mapStateToProps = (state) => ({
+    theme: state.theme.theme,
+});
+
+export default connect(mapStateToProps)(HomePage);
+
+// 成功控制状态栏
+// https://blog.csdn.net/Smallwhitestrive/article/details/132539670
+
